750dff6930fdafb5c695be42808a33c3de159e9f
sdlyyxy <sdlyyxy@icloud.com>
2017-09-18 15:34:20 +0800

    Segment fault

--- include/cppjsconsole.h
+++ include/cppjsconsole.h
@@ -8,11 +8,13 @@
 #include <ctime>
 #include <cstdlib>
 #include <typeinfo>
+#include <sstream>
 
 class Console{
     private:
         std::map<std::string,void*> typeToMap;
         int numOfGroups=0;
+        bool newline=true;
         std::string groupTab="    ";
     public:
         template <typename T>
@@ -21,21 +23,32 @@ class Console{
             typeToMap[typeid(T).name()]=(void*)&paraToTime;
             paraToTime[foo]=clock();
         }
-        template <typename T>
-        void timeEnd(T foo){
-            std::map<T,int> paraToTime=*((std::map<T,int>*)typeToMap[typeid(T).name()]);
-            int preTime=paraToTime[foo];
-            std::cout<<foo;
-            int output=(clock()-preTime+.0)/CLOCKS_PER_SEC*1000;
-            printf(": %dms\n",output);
-        }
         template <typename HEAD,typename... TYPES>
         void log(HEAD head,TYPES... tail){
+            if(newline)for(int i=0;i<numOfGroups;i++)
+                std::cout<<groupTab;
+            newline=false;
             std::cout<<head<<' ';
             this->log(tail...);
         }
         void log(){
             puts("");
+            newline=true;
+        }
+        template <typename T>
+        void timeEnd(T foo){
+            // std::string hehe;
+            std::map<T,int> paraToTime=*((std::map<T,int>*)typeToMap[typeid(T).name()]);
+            int preTime=paraToTime[foo];
+            // std::cout<<foo;
+            int output=(clock()-preTime+.0)/CLOCKS_PER_SEC*1000;
+            // printf(": %dms\n",output);
+            std::stringstream ss;
+            ss<<foo<<": "<<output<<"ms";
+            std::string res;
+            char c;
+            while(ss>>c)res+=c;
+            this->log(res);
         }
         void clear(){
             system("clear");
@@ -46,22 +59,32 @@ class Console{
                 if(!flag)throw foo;
             }
             catch(T){
-                printf("Assertion failed: ");
-                std::cout<<foo<<std::endl;
+                this->log("Assertion failed: ",foo);
+                // std::cout<<foo<<std::endl;
             }
         }
-        template <typename T>
-        void debug(T foo){
-            this->log(foo);
+        template <typename... T>
+        void debug(T... foo){
+            this->log(foo...);
         }
-        template <typename T>
-        void error(T foo){
-            printf("Console.error: ");
-            this->log(foo);
+        void debug(){
+            this->log();
         }
-        template <typename T>
-        void exception(T foo){
-            this->error(foo);
+        template <typename... T>
+        void error(T... foo){
+            // printf("console.error: ");
+            this->log("console.error: ",foo...);
+        }
+        void error(){
+            // printf("console.error: \n");
+            this->log("console.error: ");
+        }
+        template <typename... T>
+        void exception(T... foo){
+            this->error(foo...);
+        }
+        void exception(){
+            this->error();
         }
         template <typename T>
         void group(T foo){
@@ -72,15 +95,23 @@ class Console{
             numOfGroups--;
             if(numOfGroups<0)numOfGroups=0;
         }
-        template <typename T>
-        void info(T foo){
-            printf("console.info: ");
-            this->log(foo);
+        template <typename... T>
+        void info(T... foo){
+            // printf("console.info: ");
+            this->log("console.info: ",foo...);
         }
-        template <typename T>
-        void warn(T foo){
-            printf("console.warn: ");
-            this->log(foo);
+        void info(){
+            // printf("console.info: \n");
+            this->log("console.info: ");
+        }
+        template <typename... T>
+        void warn(T... foo){
+            // printf("console.warn: ");
+            this->log("console.warn: ",foo...);
+        }
+        void warn(){
+            // printf("console.warn: \n");
+            this->log("console.warn: ");
         }
         void trace(){
 
--- src/test.cpp
+++ src/test.cpp
@@ -10,8 +10,24 @@ int main(){
 	console.time('g');
 	console.timeEnd("loop");
 	//cout<<"orz"<<endl;
+	console.time(4);
 	console.log("hehe");
 	console.timeEnd('g');
-	console.assert(false,"hehe");
+	console.group("log type test");
+	console.assert(true,"hehe");
+	console.info("info message");
+	console.log();
+	console.warn();
+	console.error("hehe","error");
+	console.group("nothing");
+	console.log("hehe");
+	console.timeEnd(4);
+	console.groupEnd();
+	console.log("132");
+	console.groupEnd();
+	console.log("132");
+	console.groupEnd();
+	console.log("132");
+	
 	return 0;
 }
